ff68c32eef3f46fafa00685aa5e29942df1b86ea
Dajen <tech-with-dajen@github.com>
2026-06-28 23:52:48 +0200

    trying to fix github-file error

--- src/main/Java/openchess/Board.java
+++ src/main/Java/openchess/Board.java
@@ -1,19 +1,50 @@
 package main.java.openchess;
-from main.java.openchess.Piece import Piece;
-public class Board {
-    public static void main(String[] args) {
-        // Backend representation of the chess board using a 2D array
-        // Each number represents a piece, with negative numbers for black pieces and positive numbers for white pieces
-        // The pieces are represented as follows:
-        // 1: Pawn, 7: Knight, 6: Bishop, 8: Rook, 5: Queen, 4: King
-        int[][] Board = {{-8, -7, -6, -5, -4, -6,-7,-8},
-                         {-1 ,-1, -1, -1, -1, -1,-1,-1},
-                         { 0,  0,  0,  0,  0,  0 ,0,0},
-                         { 0,  0,  0,  0,  0,  0 ,0,0},
-                         { 0,  0,  0,  0,  0,  0 ,0,0},
-                         { 0,  0,  0,  0,  0,  0 ,0,0},
-                         { 1,  1,  1,  1,  1,  1, 1,1},
-                         { 8,  7,  6,  5,  4,  6, 7,8}};
-                
-    }
-}
+public class Board{ 
+        Piece[][] board = new Piece[8][8];
+
+        public  Board() {
+            resetBoard();
+        }
+
+        private void resetBoard() {
+        //Board setup
+        
+        board[0][0] = new Piece(Piece.Type.ROOK, Piece.Color.BLACK);
+        board[0][1] = new Piece(Piece.Type.KNIGHT, Piece.Color.BLACK);
+        board[0][2] = new Piece(Piece.Type.BISHOP, Piece.Color.BLACK);
+        board[0][3] = new Piece(Piece.Type.QUEEN, Piece.Color.BLACK);
+        board[0][4] = new Piece(Piece.Type.KING, Piece.Color.BLACK);
+        board[0][5] = new Piece(Piece.Type.BISHOP, Piece.Color.BLACK);
+        board[0][6] = new Piece(Piece.Type.KNIGHT, Piece.Color.BLACK);
+        board[0][7] = new Piece(Piece.Type.ROOK, Piece.Color.BLACK);
+        board[1][0] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        board[1][1] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        board[1][2] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        board[1][3] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        board[1][4] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        board[1][5] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        board[1][6] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        board[1][7] = new Piece(Piece.Type.PAWN, Piece.Color.BLACK);
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = null; // Empty squares
+            }
+        }
+        board[6][0] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[6][1] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[6][2] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[6][3] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[6][4] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[6][5] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[6][6] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[6][7] = new Piece(Piece.Type.PAWN, Piece.Color.WHITE);
+        board[7][0] = new Piece(Piece.Type.ROOK, Piece.Color.WHITE);
+        board[7][1] = new Piece(Piece.Type.KNIGHT, Piece.Color.WHITE);
+        board[7][2] = new Piece(Piece.Type.BISHOP, Piece.Color.WHITE);
+        board[7][3] = new Piece(Piece.Type.QUEEN, Piece.Color.WHITE);
+        board[7][4] = new Piece(Piece.Type.KING, Piece.Color.WHITE);
+        board[7][5] = new Piece(Piece.Type.BISHOP, Piece.Color.WHITE);
+        board[7][6] = new Piece(Piece.Type.KNIGHT, Piece.Color.WHITE);
+        board[7][7] = new Piece(Piece.Type.ROOK, Piece.Color.WHITE);
+        System.out.println("Initial board setup:");
+    }}
